16c39871d2ac93b1ca1154209e8511f3930e3672
Sunyoung Hwang <sunyounghwang13@gmail.com>
2018-08-03 20:27:10 +0000

    automatically backed up by learn

--- fizzbuzz.rb
+++ fizzbuzz.rb
@@ -1,10 +1,11 @@
 def fizzbuzz(num)
-  if num % 3 == 0 && num % 5 != 0
-    return "Fizz"
-  elsif num % 5 == 0 && num % 3 != 0
-    return "Buzz"
-  elsif num % 3 == 0 && num % 5 == 0
+  if num % 3 == 0 && num % 5 == 0
     return "FizzBuzz"
+  if num % 3 == 0
+    return "Fizz"
+  elsif num % 5 == 0
+    return "Buzz"
+
   else
     return nil
   end
